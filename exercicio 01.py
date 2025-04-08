775095044fbb0bef12318ceba437e8bbf07a4557
deyvison iure <deyvison.iure@gmai.com>
2025-04-08 09:00:25 -0300

    horario hora

--- exercicio 01.py
+++ exercicio 01.py
@@ -1,14 +1,27 @@
-#o primeiro  horario recebe em 24h
-hora_1=int(input("hora: "))
-minuto_1=int(input("minnuto: "))
-
-if minuto_1 == 60:
-    hora_1= hora_1+1
-    minuto_1=minuto_1-60
+hora1=15
+minuto1=1
+hora2=92
+minuto2=8
+hora_final=hora1+hora2
+minuto_final=minuto2+minuto1
+if hora_final>=12:
+    hora_final=hora_final-(hora_final//12)*12
+if minuto_final>=60:
+    hora_final=hora_final+(minuto_final//60)
+    minuto_final=minuto_final-(minuto_final//60*60)
+    if minuto_final >=10:
+        print(f"hora final= {hora_final}:{minuto_final}h")
+    else:
+        print(f"hora final= {hora_final}:0{minuto_final}")
 else:
-    if minuto_1 > 60:
-        hora_1 = hora_1 + 1
-        minuto_1=minuto_1-60
-print(f"{hora_1}:{minuto_1}")
+    if minuto_final >=10:
+        print(f"hora final= {hora_final}:{minuto_final}")
+    else:
+        print(f"hora final= {hora_final}:0{minuto_final}")
+
+
+
+
+
 
 
